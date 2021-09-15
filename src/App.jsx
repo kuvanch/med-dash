7ef4ca1ab9a6d9245d2991df4aaac9eddfdbf1b3
kannadurdiev <kolyabek13@gmail.com>
2021-09-15 14:17:50 +0500

    added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Layout from 'antd/lib/layout/layout';
-import {Sidebar,HeaderPage} from './components'
+import {Sidebar,HeaderPage, ContentPage} from './components'
 import { useState } from 'react';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'antd/dist/antd.css'; 
 import './App.css'
+import { Home } from './page/Home';
+import { Login } from './page/Login';
 
 
 function App() {
@@ -13,14 +15,23 @@ function App() {
   const handleTrigger = () => {
     setCollapsed(!collapsed)
   }
-
+  const home = <Layout className='app'>
+                  <Router>
+                    <Sidebar collapsed={collapsed}/>
+                    <Layout className="site-layout">
+                      <HeaderPage handleTrigger={handleTrigger} collapsed={collapsed}/>
+                      <ContentPage>
+                          <Switch>
+                            <Route path='/home' component={Home}/>
+                          </Switch>
+                      </ContentPage>
+                    </Layout>
+                  </Router>
+                </Layout>
   return (
-    <Layout className='app'>
-      <Sidebar collapsed={collapsed}/>
-      <Layout className="site-layout">
-        <HeaderPage handleTrigger={handleTrigger} collapsed={collapsed}/>
-      </Layout>
-    </Layout>
+    <>
+      {true ? home : <Login/>}
+    </>
   );
 }
 
---
+++ src/page/Login.jsx
@@ -0,0 +1,16 @@
+import { Card } from 'antd'
+import Form from 'antd/lib/form/Form'
+import Layout from 'antd/lib/layout/layout'
+import React from 'react'
+
+export const Login = () => {
+    return (
+        <Layout className='login'>
+            <Card>
+                <Form>
+                    
+                </Form>
+            </Card>
+        </Layout>
+    )
+}
