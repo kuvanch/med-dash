1495a5a4be0903c69ef2b53069cff2bd1c8c4a5a
kannadurdiev <kolyabek13@gmail.com>
2021-09-16 13:57:51 +0500

    set table component

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -2,60 +2,7 @@ import React from 'react';
 import {Table} from 'antd';
 
 
-export const TableComponent = () => {
-
-
-
-          const dataSource = [
-          {
-              key: '1',
-              name: 'Mike',
-              age: 32,
-              address: '10 Downing Street',
-          },
-          {
-              key: '2',
-              name: 'John',
-              age: 42,
-              address: '10 Downing Street',
-          },
-          ];
-
-          const columns = [
-              {
-                  title: "Id",
-                  dataIndex: 'name',
-                  key: 'name',
-              },
-              {
-                title: "Rasmmi",
-
-              },
-          {
-              title: "Shifohona nomi",
-              dataIndex: 'name',
-              key: 'name',
-          },
-          {
-              title: 'Manzil',
-              dataIndex: 'age',
-              key: 'age',
-          },
-          {
-              title: 'Telefon raqam',
-              dataIndex: 'address',
-              key: 'address',
-          },
-              {
-                title: 'Shifohona admin',
-              },
-              {
-                  title: "Geolakatsiya ma'lumoti"
-              }
-          ];
-
+export const TableComponent = ({dataSource,columns}) => {
         return  <Table dataSource={dataSource} columns={columns} />;
-
-
 };
 
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -4,6 +4,53 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { TableComponent } from '../components'
 export const Home = () => {
+    const dataSource = [
+        {
+            key: '1',
+            name: 'Mike',
+            age: 32,
+            address: '10 Downing Street',
+        },
+        {
+            key: '2',
+            name: 'John',
+            age: 42,
+            address: '10 Downing Street',
+        },
+        ];
+
+        const columns = [
+            {
+                title: "Id",
+                dataIndex: 'name',
+                key: 'name',
+            },
+            {
+              title: "Rasmmi",
+
+            },
+        {
+            title: "Shifohona nomi",
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Manzil',
+            dataIndex: 'age',
+            key: 'age',
+        },
+        {
+            title: 'Telefon raqam',
+            dataIndex: 'address',
+            key: 'address',
+        },
+            {
+              title: 'Shifohona admin',
+            },
+            {
+                title: "Geolakatsiya ma'lumoti"
+            }
+        ];
     return (
         <Row gutter={[20,20]}>
             <Col span={12}>
@@ -46,7 +93,7 @@ export const Home = () => {
             </Col>
             <Col span={24}>
                 <Typography.Title level={3}>Shifoxonalar reytingi</Typography.Title>
-                <TableComponent/>
+                <TableComponent dataSource={dataSource} columns={columns}/>
             </Col>
             
         </Row>
